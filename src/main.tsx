a241f16f899eceba6e8fa433f374ee7a77886537
soliery <41116080+soliery@users.noreply.github.com>
2026-04-09 23:42:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Portfolio from "./Portfolio";
+import Portfolio from "./portfolio";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
