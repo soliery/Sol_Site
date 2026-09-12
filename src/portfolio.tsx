28e1bc6f5da26f1f859125b089b4faaafae7036b
soliery <41116080+soliery@users.noreply.github.com>
2026-09-12 21:08:51 +0100

    Update portfolio.tsx

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -115,7 +115,7 @@ const Header = () => (
     <div className="absolute left-8 -bottom-16 flex items-end gap-6">
       <div className="w-32 h-32 rounded-2xl bg-gray-500 border-4 border-[#0a0705] shadow-2xl overflow-hidden">
         <img 
-          src="/avatar.png" 
+          src="/avatar.jpg" 
           alt="Val Sol" 
           className="w-full h-full object-cover" 
         />
